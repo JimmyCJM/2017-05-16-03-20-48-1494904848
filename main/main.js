6b7706ccbce16c9113e35cfd27ba6968f23faa0b
JimmyCJM <chijieming@sina.com>
2017-06-24 23:44:25 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,6 +1,33 @@
 const loadAllItems = require('./loadAllItems.js');
 
+var menu = loadAllItems();
+var inputs = [
+            'ITEM000000',
+            'ITEM000000',
+            'ITEM000000',
+            'ITEM000000',
+            'ITEM000000',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000004'
+        ];
+
 module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+    console.log('***<没钱赚商店>购物清单***\n') 
+   var CNum=0,SNum=0,BNum=0;
+   for(let i=0;i<inputs.length;i++) {
+        switch(inputs[i]) {
+            case 'ITEM000000':{
+                CNum++;
+                break;
+            }
+            case 'ITEM000001':{
+                SNum++;
+                break;
+            }
+            case 'ITEM000004':{
+                BNum++;
+                break;
+            }
+        }
 };
